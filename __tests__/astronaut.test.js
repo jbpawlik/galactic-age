ebaa4fc57e3777d4730736abb8081066cbe78799
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-25 10:09:13 -0700

    merge planetAge functions into solarAge function

--- __tests__/astronaut.test.js
+++ __tests__/astronaut.test.js
@@ -2,32 +2,29 @@
 import Astronaut from '../src/js/astronaut.js';
 
 describe ('Astronaut', () => {
-  let astronaut = new Astronaut(70);
+  let astronaut = new Astronaut();
+  astronaut.solarAge(70);
 
   beforeEach(() => {
   });
 
   test('Should create astronaut object with age property', () => {
-    expect(astronaut.age).toBeDefined();
+    expect(astronaut.earthYears).toBeDefined();
   });
 
   test('Should calculate age in Mercury years', () => {
-    astronaut.mercuryAge(70);
     expect(astronaut.mercuryYears).toEqual(290);
   });
 
   test('Should calculate age in Venus years', () => {
-    astronaut.venusAge(70);
     expect(astronaut.venusYears).toEqual(113);
   });
 
   test('Should calculate age in Mars years', ()=> {
-    astronaut.marsAge(70);
     expect(astronaut.marsYears).toEqual(37);
   });
 
   test('Should calculate age in Jupiter years', ()=> {
-    astronaut.jupiterAge(70);
     expect(astronaut.jupiterYears).toEqual(5);
   });
 
--- src/js/astronaut.js
+++ src/js/astronaut.js
@@ -1,21 +1,29 @@
 export default class Astronaut {
-  constructor(age) {
-    this.age = age;
+  constructor() {
+
   }
 
-  mercuryAge(age) {
+  solarAge(age) {
+    this.earthYears = age;
     this.mercuryYears = parseInt(age * 365.25 / 88);
-  }
-
-  venusAge(age) {
     this.venusYears = parseInt(age * 365.25 / 225);
-  }
-
-  marsAge(age) {
     this.marsYears = parseInt(age * 365.25 / 687);
-  }
-
-  jupiterAge(age) {
     this.jupiterYears = parseInt(age * 365.25 / 4383);
   }
 }
+//   mercuryAge(age) {
+//     this.mercuryYears = parseInt(age * 365.25 / 88);
+//   }
+
+//   venusAge(age) {
+//     this.venusYears = parseInt(age * 365.25 / 225);
+//   }
+
+//   marsAge(age) {
+//     this.marsYears = parseInt(age * 365.25 / 687);
+//   }
+
+//   jupiterAge(age) {
+//     this.jupiterYears = parseInt(age * 365.25 / 4383);
+//   }
+// }
