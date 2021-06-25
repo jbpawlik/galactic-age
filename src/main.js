f98555b6a48d8f12ce73b20c143e194ca1271b5f
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-25 15:01:23 -0700

    begin hooking UI logic to UI

--- src/main.js
+++ src/main.js
@@ -4,3 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Astronaut from './js/astronaut.js';
 import $ from 'jquery';
+
+//User Interface Logic:
+
+
+
+
+$('#workerInfoButton').click(function() {
+  let astronaut = new Astronaut();
+  let employeeAge = $('#ageField').val();
+  astronaut.solarAge(employeeAge);
+  astronaut.lifeExpectancy();
+  astronaut.lifeExpectancyFactors();
+  astronaut.yearsToDeath(employeeAge);
+  let region = $('input:radio:checked[name:region]').val();
+  let health = $('input:radio:checked[name:health]').val();
+  let happiness = $('input:radio:checked[name=happiness]').val();
+  let employeeName = $('#nameField').val();
+  
+  $("#galacticYears").text('<li> Earth:' + astronaut.earthYears + '</li>')
+
+
+});
