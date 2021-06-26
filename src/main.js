d0eb324ceaf71f4ddff9a5bb5fbff5ccb71f28a3
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-26 08:10:14 -0700

    add alert if lifespan exceeds average

--- src/main.js
+++ src/main.js
@@ -27,12 +27,18 @@ $('#workerInfoButton').click(function(event) {
   astronaut.solarAge(employeeAge);
   astronaut.yearsToDeath(employeeAge);
   let employeeName = $('#nameField').val();
+  let longLife = astronaut.longLifeChecker();
 
   $('#nameOutput').html(employeeName);
 
-  $('#solarYears').html('<li> Earth: ' + astronaut.earthYears + ' Years Old </li>' + '<li> Mercury: ' + astronaut.mercuryYears + ' Years Old </li>' + '<li> Venus: ' + astronaut.venusYears + ' Years Old </li>' + '<li> Mars: ' + astronaut.marsYears + ' Years Old </li>' + '<li> Jupiter: ' + astronaut.jupiterYears + ' Years Old');
-  
-  $('#solarExpectancy').html('<li> Earth: ' + parseInt(astronaut.earthLifeExpectancy) + ' Years Old </li>' + '<li> Mercury: ' + astronaut.mercuryLifeExpectancy + ' Years Old </li>' + '<li> Venus: ' + astronaut.venusLifeExpectancy + ' Years Old </li>' + '<li> Mars: ' + astronaut.marsLifeExpectancy + ' Years Old </li>' + '<li> Jupiter: ' + astronaut.jupiterLifeExpectancy + ' Years Old');
-  
-  $('#workLifespan').html('<li>' + parseInt(astronaut.earthYearsLeft) + ' years</li><li>' + astronaut.mercuryYearsLeft + ' years</li><li>' + astronaut.venusYearsLeft + ' years</li><li>' + astronaut.marsYearsLeft + ' years</li><li>' + astronaut.jupiterYearsLeft + ' years</li>');
+  $('#planetYears').html('<li> Earth: ' + astronaut.earthYears + ' Years Old </li>' + '<li> Mercury: ' + astronaut.mercuryYears + ' Years Old </li>' + '<li> Venus: ' + astronaut.venusYears + ' Years Old </li>' + '<li> Mars: ' + astronaut.marsYears + ' Years Old </li>' + '<li> Jupiter: ' + astronaut.jupiterYears + ' Years Old');
+
+  $('#planetExpectancy').html('<li> Earth: ' + parseInt(astronaut.earthLifeExpectancy) + ' Years Old </li>' + '<li> Mercury: ' + astronaut.mercuryLifeExpectancy + ' Years Old </li>' + '<li> Venus: ' + astronaut.venusLifeExpectancy + ' Years Old </li>' + '<li> Mars: ' + astronaut.marsLifeExpectancy + ' Years Old </li>' + '<li> Jupiter: ' + astronaut.jupiterLifeExpectancy + ' Years Old');
+
+  if (longLife === false) {
+    $('#workLifespan').html('<li>' + parseInt(astronaut.earthYearsLeft) + ' years</li><li>' + astronaut.mercuryYearsLeft + ' years</li><li>' + astronaut.venusYearsLeft + ' years</li><li>' + astronaut.marsYearsLeft + ' years</li><li>' + astronaut.jupiterYearsLeft + ' years</li>');
+  } else {
+    $('#workLifespan').html('<li>ALERT!</li> POTENTIAL <li></li>PROFIT<li>OPPORTUNITY</li><li>SEE NOTE</li><li></li><li></li>');
+    $('#lifespanAlert').show();
+  }
 });
